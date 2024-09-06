2bce69426d5e145ee0010057f15713a14199ace8
santhoshdass <santhoshcse2019@gmail.com>
2024-09-06 15:54:40 +0530

    medusafieladded

--- medusa-ecs-terraformm/main.tf
+++ medusa-ecs-terraformm/main.tf
@@ -1,3 +1,77 @@
-provider "aws" {
-  region = "us-east-1"
+resource "aws_vpc" "medusa_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.medusa_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.medusa_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_internet_gateway" "medusa_igw" {
+  vpc_id = aws_vpc.medusa_vpc.id
+}
+
+resource "aws_ecs_cluster" "medusa_cluster" {
+  name = "medusa-cluster"
+}
+
+resource "aws_ecs_task_definition" "medusa_task" {
+  family                   = "medusa-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name  = "medusa-container"
+      image = "<ECR-Image-URL>"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "medusa_service" {
+  name            = "medusa-service"
+  cluster         = aws_ecs_cluster.medusa_cluster.id
+  task_definition = aws_ecs_task_definition.medusa_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = [aws_subnet.public_subnet.id]
+    security_groups = [aws_security_group.ecs_sg.id]
+  }
+}
+
+resource "aws_lb" "medusa_alb" {
+  name               = "medusa-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.public_subnet.id]
+}
+
+resource "aws_lb_listener" "medusa_listener" {
+  load_balancer_arn = aws_lb.medusa_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.medusa_tg.arn
+  }
 }
---
+++ medusa-ecs-terraformm/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  description = "The AWS region to deploy the resources"
+  default     = "us-east-1"
+}
+
+variable "app_name" {
+  description = "The name of the application"
+  default     = "medusa-backend"
+}
